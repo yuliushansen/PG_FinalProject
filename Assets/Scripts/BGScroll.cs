297f90bc2e85a863cf52c3f47a710b8ae7c538f9
rius1212 <riussabarno12@gmail.com>
2019-11-15 13:25:25 +0700

    Tambahin Background

---
+++ Assets/Scripts/BGScroll.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+    public float scroll_Speed = 0.1f;
+    private MeshRenderer mesh_Renderer;
+
+    private Vector2 saved_Offset;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mesh_Renderer = GetComponent<MeshRenderer>();
+        saved_Offset = mesh_Renderer.sharedMaterial.GetTextureOffset("_MainTex");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Time.time * scroll_Speed;
+        Vector2 offset = new Vector2(x,0);
+        mesh_Renderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+
+    void OnDisablae(){
+        mesh_Renderer.sharedMaterial.SetTextureOffset("_MainTex", saved_Offset);
+    }
+}
